a01532ab9473d9bb6bed11ce209d99030966dff4
Kheiner Ivan Murillo Mosquera <kheinermurillomosquera@gmail.com>
2022-01-28 02:22:12 -0500

    all tests ok

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -118,7 +118,8 @@ function elevarAlCubo(num) {
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
-  // Tu código:  (alternative: Math.pow(num, exponent))
+  // Tu código:  
+  // alternative: Math.pow(num, exponent)
   return num ** exponent;
 }
 
